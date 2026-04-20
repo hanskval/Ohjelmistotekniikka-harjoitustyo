d96e7ca7bda63a44835157aa1af11a49b667f3d9
hanskval <valtteri.hanska@helsinki.fi>
2026-04-20 13:33:55 +0300

    fixed coverage report task

--- tasks.py
+++ tasks.py
@@ -6,6 +6,15 @@ def start(ctx):
 @task
 def build(ctx):
     ctx.run("python3 -m src.build")
+
 @task
+def coverage(ctx):
+    ctx.run("coverage run -m --branch -m pytest src")
+    
+@task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage report -m")
+    ctx.run("coverage report")
+    
+@task
+def lint(ctx):
+    ctx.run("pylint src")
